62ba01b11086287cac51bd0a6402d5f0650841f9
Matheus <reboucas444@gmail.com>
2025-07-01 09:06:30 -0300

    writing the add function and getting the information through get element by id but using a strategy of not repeating code by assigning it to a function with the help of bind(document)

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function adicionar() {
+     //recuperar valores do nome deo produto, quantidade e valores
+     let getElement = document.getElementById.bind(document);
+
+     let nomeProduto = getElement('produto');
+     let quantidade = getElement('quantidade');
+
+     alert(nomeProduto.value, quantidade.value);
+     //calcular o preço, o subtotal
+
+     //adicionar ao carrinho
+
+     //atualizar valor total
+}
+
+function limpar() {
+
+}
